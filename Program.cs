3dcd14047449f4a461e519fa257b98c04b64c593
Danny227 <dplesko2007@gmail.com>
2025-09-26 19:06:13 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Runtime.InteropServices.ObjectiveC;
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Danil";
+            //int age = 18;
+            //string city = "Volgograd";
+            //Console.WriteLine("ваше имя {0}, ваш возраст {1}, ваш город {2}", name, age, city)
+            //byte bit1 = 1;
+            //byte bit2 = 255;
+            //byte bit3 = 464643125;
+            //int number = 121212;
+            //Int128 number5 = 12345678;
+
+            //System.Int32 number6 = 45727;
+
+            //bool alive = true;
+            //bool isdead = false;
+
+            //float floating1 = 3.14f;
+            //double yNumber = 3.4353;
+            //decimal bb = 4.2542675m;
+
+            //string name = "Danya";
+            //char symbol = 'A';
+
+            //object obl = 3.3;
+            //Console.WriteLine(int.MinValue);
+            //Console.WriteLine(int.MaxValue);
+
+            //Console.WriteLine("Введите Ваше имя: ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"Ваше имя -- {name}");
+            //Console.WriteLine("youe age: ");
+            //int age = int.Parse
+            //    ( Console.ReadLine() );
+            //Console.WriteLine($"your age is {age} y.o.");
+            //var number = 10;
+            //byte bit1 = 1;
+            //byte bit2 = 100;
+            //sbyte bit3 = -101;
+            //sbyte bit4 = 102;
+            //short n1 = 1;
+            //short n2 = 102;
+            //ushort n3 = 1;
+            //ushort n4 = 102;
+            //int integer1 = 10;
+            //int integer2 = 0b101;
+            //uint integer3 = 10;
+            //int integer4 = 0b101;
+            //long number1 = -10;
+            //long number2 = 0b101;
+            //ulong number3 = 10;
+            //ulong number4 = 0b101;
+            //bool alive = true;
+            //bool isDead = false;
+            //float floating1 = 3.14F;
+            //float floating2 = 30.6f;
+            //double yNumber1 = 3.14;
+            //double yNumber2 = 36.6;
+            //decimal dNumber1 = 4.5m;
+            //decimal dNumber2 = 23.56M;
+            //char s = 'A';
+            //char d = ' ';
+            //string hello = "hello";
+            //string word = "world";
+            //object ob1 = 22;
+            //object ob2 = 3.14;
+            //object ob3 = "hello code";
+            //string name = null;
+            //Console.WriteLine(name);
+            //Console.WriteLine(int.MaxValue);
+            //Console.WriteLine(int.MinValue);
+            //Console.WriteLine(double.MaxValue);
+            //Console.WriteLine(double.MinValue);
+            //string numStr = "123";
+            //string nullStr = null;
+            //int number1 = Convert.ToInt32(numStr);
+            //int number2 = int.Parse(nullStr);
+            //int metr1 = 1;
+            //System.Int32 metr2 = 1;
+            //var number = 10;
+            //var symbol = 'c';
+            //var greeting = "Hello";
+            //string hello = "Hello world";
+            //Console.WriteLine(hello);
+            //Console.WriteLine("Я изучаю C#");
+            //Console.WriteLine("Goodbye World...");
+            //Console.WriteLine(36.6);
+        }
+    }
+}
